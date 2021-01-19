452379e97aff30e79d3e119002f411d3e9462328
Mateusz Groblicki <lokigameplayer@gmail.com>
2021-01-19 19:34:24 +0100

    initial commit

---
+++ StoreWebApp/Models/Article.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Lista12.Models
+{
+    public class Article
+    {
+        public int ID { get; set; }
+
+        [StringLength(100, MinimumLength = 3)]
+        [Required]
+        public string Name { get; set; }
+
+        [DataType(DataType.Currency)]
+        [Required]
+        public decimal Price { get; set; }
+
+        [Display(Name = "Image path")] public string ImagePath { get; set; }
+
+        public int CategoryId { get; set; }
+        public Category Category { get; set; }
+    }
+}
---
+++ StoreWebApp/Models/Category.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Lista12.Models
+{
+    public class Category
+    {
+        public int ID { get; set; }
+        [StringLength(100)] public string Name { get; set; }
+        public IList<Article> Articles { get; set; }
+    }
+}
